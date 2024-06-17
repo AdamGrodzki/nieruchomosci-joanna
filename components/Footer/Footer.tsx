dbb9f270318e69974a250114bc839f081fe2a6c4
Adam Grodzki <110930032+AdamGrodzki@users.noreply.github.com>
2024-06-17 14:49:02 +0200

    update styles and responsive design for footerPage, fix navbar styles

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,39 +1,50 @@
 import styles from "./footer.module.scss"
-import { FaFacebookF, } from "react-icons/fa";
+import logo from "@/images/logoDark.png"
+import { FaFacebook } from "react-icons/fa";
 import { BsTelephone } from "react-icons/bs";
 import { MdOutlineMail, MdOutlineLocationOn } from "react-icons/md";
+import Link from "next/link";
+import Image from "next/image";
+
 
 
 const Footer = () => {
     return ( 
+    <>
         <footer className={styles.footer}>
-        <div className={styles.footerContent}>
             <div className={styles.footerSection}>
-                <h4>Skontaktuj Się Z Nami</h4>
+                <div className={styles.footerLogo}>
+                    <Link href="/">
+                        <Image
+                            src={logo}
+                            width={250}
+                            height={100}
+                            alt="logo" />
+                    </Link>
+                    <p>Joanna Nieruchomości</p>
+                </div>
                 <ul>
-                    <li>Nieruchomosci Joanna</li>
-                    <li><MdOutlineLocationOn /> Tczew, Poland 83-110</li>
-                    <li><BsTelephone /> <a  href="tel: 123 456 789"> 123 456 789</a></li>
+            <h4>Skontaktuj Się Z Nami</h4>
+                    <li><MdOutlineLocationOn /><a target="_blank" href="https://maps.app.goo.gl/5uZEP1JBUwQGHTfZ8"> Tczew, Poland 83-110</a></li>
+                    <li><BsTelephone /> <a href="tel: 123 456 789"> 123 456 789</a></li>
                     <li><BsTelephone /> <a href="tel: 987 654 321"> 987 654 321</a></li>
                     <li><MdOutlineMail /> <a href="mailto:joanna@estateapp.com"> joanna@estateapp.com</a></li>
                     <li><MdOutlineMail /> <a href="mailto:sebastian@estateapp.com"> sebastian@estateapp.com</a></li>
                 </ul>
-            </div>
-        
-            <div className={styles.footerSection}> 
-                <h4>Obserwuj nas</h4>
                 <div className={styles.socialMedia}>
-                    <a href="https://facebook.com" aria-label="Facebook" target="_blank" rel="noopener noreferrer">
-                    <FaFacebookF className={styles.facebookIcon}/>
-                    </a>
+            <h4>Obserwuj nas</h4>
+                    <Link href="https://facebook.com" aria-label="Facebook" target="_blank">
+                        <FaFacebook  className={styles.facebookIcon} />
+                    </Link>
                 </div>
             </div>
-        </div>
-        <div>
-            &copy; 2024 Wszystkie prawa zastrzeżone | Joanna nieruchomości 
-        </div>
-    </footer>
+        </footer>
+            <div className={styles.footerBottom}>
+                &copy; 2024 Wszystkie prawa zastrzeżone | Joanna nieruchomości
+            </div>
+    </>
      );
 }
  
 export default Footer;
+
