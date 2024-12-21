3ab6e8ad9a40744db4caa4545607e2aef09e2522
Adam Grodzki <110930032+AdamGrodzki@users.noreply.github.com>
2024-12-21 16:35:05 +0100

    add global loader for content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,62 @@
 import "@/styles/globals.scss";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import Layout from "@/components/LayoutPage/Layout";
 import LandingPage from "@/components/LandingPage/LandingPage";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import PropertyTiles from "@/components/PropertyTiles/PropertyTiles";
 
-const App = ({Component, pageProps}: AppProps) => {
+const Loader = () => (
+    <div style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        zIndex: 9999
+    }}>
+        <div className="spinner"></div>
+    </div>
+);
+
+const App = ({ Component, pageProps }: AppProps) => {
     const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
     const isHomePage = router.pathname === "/";
+
+    useEffect(() => {
+        const handleStart = () => setIsLoading(true);
+        const handleComplete = () => setIsLoading(false);
+
+        router.events.on("routeChangeStart", handleStart);
+        router.events.on("routeChangeComplete", handleComplete);
+        router.events.on("routeChangeError", handleComplete);
+
+        return () => {
+            router.events.off("routeChangeStart", handleStart);
+            router.events.off("routeChangeComplete", handleComplete);
+            router.events.off("routeChangeError", handleComplete);
+        };
+    }, [router]);
 
     return (
         <Layout>
-            {isHomePage && (
+            {isLoading && <Loader />}
+            {!isLoading && isHomePage && (
                 <>
                     <LandingPage />
                     <SearchBar />
                     <PropertyTiles />
                 </>
             )}
-            <Component {...pageProps} />
+            {!isLoading && <Component {...pageProps} />}
         </Layout>
-     );
-}
- 
+    );
+};
+
 export default App;
